c15670c836e2b88e9390b1a4308e3909fdf54c57
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-01-24 08:48:03 +0000

    refactored the code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,11 @@
 class FizzBuzz
 
-  def numbers
-    (1..100).to_a
+  #similar to a constructor so when we call FizzBuzz.new it creates an instance of this class and then calls this method on the new object
+  def initialize
+    @numbers = (1..100).to_a
   end
 
-  def sequence
+  def fizzbuzz
     numbers.collect do |num|
       if ((num % 3 == 0) and (num % 5 == 0))
         'FizzBuzz' 
@@ -18,6 +19,10 @@ class FizzBuzz
         num
       end
     end
+  end
+
+  def numbers
+    (1..100).to_a
   end
 
 end
