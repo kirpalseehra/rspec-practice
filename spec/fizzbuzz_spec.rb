7683b7acce72fdd52b7ad27adf759379ad4f5479
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-01-24 09:55:28 +0000

    refactored more code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,27 +1,21 @@
 require 'fizzbuzz'
 
-# before(:all) do 
-#   @fizzy = FizzBuzz.new 
-# end
-
 describe FizzBuzz do
 
+before(:all) do 
+  @fizzy = FizzBuzz.new 
+end
+
   it 'should print Fizz for multiples of 3' do
-    game = FizzBuzz.new
-    result = game.fizzbuzz
-    expect(result[98]).to eq('Fizz') 
+    expect(@fizzy.fizzbuzz[98]).to eq('Fizz') 
   end
 
   it 'should print Buzz for multiples of 5' do
-    game = FizzBuzz.new
-    result = game.fizzbuzz
-    expect(result[4]).to eq('Buzz')
+    expect(@fizzy.fizzbuzz[4]).to eq('Buzz')
   end
 
   it 'should print FizzBuzz for multiples of 3 and 5' do
-    game = FizzBuzz.new
-    result = game.fizzbuzz
-    expect(result[14]).to eq('FizzBuzz')
+    expect(@fizzy.fizzbuzz[14]).to eq('FizzBuzz')
   end
 
 
